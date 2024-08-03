3b46b8a27018e5633f93045eacdd66cf36abf6fc
Ramadevi <ramadevi@Ramadevis-MacBook-Air.local>
2024-08-03 22:20:39 +0530

    CRUD operations

---
+++ Infrastructure/Migrations/20240803153518_Update.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    public partial class Update : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ContryName",
+                table: "Countries",
+                newName: "CountryName");
+
+            migrationBuilder.RenameColumn(
+                name: "ContryCode",
+                table: "Countries",
+                newName: "CountryCode");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CountryName",
+                table: "Countries",
+                newName: "ContryName");
+
+            migrationBuilder.RenameColumn(
+                name: "CountryCode",
+                table: "Countries",
+                newName: "ContryCode");
+        }
+    }
+}
---
+++ SCA/Controllers/SCAController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Domain.Interface;
+using Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Application.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SCAController:ControllerBase
+	{
+		private readonly ISCAService _iscaservice;
+
+		public SCAController(ISCAService iscaservice)
+		{
+			_iscaservice = iscaservice;
+		}
+		[HttpPut]
+		public string CreateCountry(CountryDTO countrydto)
+		{
+			var result=_iscaservice.CreateCountry(countrydto);
+			return result;
+			
+		}
+		[HttpPost]
+		public string CreateStore(StoreDTO storedto)
+		{
+			var result = _iscaservice.CreateStore(storedto);
+			return result;
+		}
+		
+	}
+}
+
